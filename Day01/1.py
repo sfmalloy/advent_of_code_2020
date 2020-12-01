c0bb09a613abc002d2a1de32909a4650b3a58b6e
Sean Malloy <sfmalloy@millersville.edu>
2020-12-01 11:03:16 -0500

    Day 1 shortened

--- Day01/1.py
+++ Day01/1.py
@@ -1,13 +1,4 @@
 with open('1.in') as f:
-  lines = [int(x) for x in f.readlines()]
-
-for i in range(len(lines)):
-  for j in range(i, len(lines)):
-    if lines[i] + lines[j] == 2020:
-      print(lines[i]*lines[j])
-
-for i in range(len(lines)):
-  for j in range(i, len(lines)):
-    for k in range(j, len(lines)):
-      if lines[i] + lines[j] + lines[k] == 2020:
-        print(lines[i]*lines[j]*lines[k])
+  l = [int(x) for x in f.readlines()]
+[print((f'{l[j]*l[k]}\n' if l[j]+l[k]==2020 and i==0 else ''), (f'{l[i]*l[j]*l[k]}\n' if l[i]+l[j]+l[k]==2020 else ''), sep='', end='') 
+  for i in range(len(l)) for j in range(i+1, len(l)) for k in range(j+1, len(l))]
