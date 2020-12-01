d2ec0f1ac803aebd08025d826a1d8ac0dfc6bb1f
Sean Malloy <sfmalloy@millersville.edu>
2020-12-01 17:15:08 -0500

    Final version of Day 1. Generic and big speedup

--- Day01/1.py
+++ Day01/1.py
@@ -1,4 +1,16 @@
 with open('1.in') as f:
-  l = [int(x) for x in f.readlines()]
-[print((f'{l[j]*l[k]}\n' if l[j]+l[k]==2020 and i==0 else ''), (f'{l[i]*l[j]*l[k]}\n' if l[i]+l[j]+l[k]==2020 else ''), sep='', end='') 
-  for i in range(len(l)) for j in range(i+1, len(l)) for k in range(j+1, len(l))]
+  l = sorted(map(int, f.readlines()))
+
+def find_sum(l, n, d, p=0, s=0):
+  if s > n:
+    return 0
+  if d == 0:
+    return int(s == n)
+  for i in range(p, len(l)):
+    prod = l[i] * find_sum(l, n, d-1, i+1, s+l[i])
+    if prod > 0:
+      return prod
+  return 0
+
+print(find_sum(l, 2020, 2))
+print(find_sum(l, 2020, 3))
