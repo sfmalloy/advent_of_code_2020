f5e6840708dec2994e036e7e5820e11d19e88a57
Sean Malloy <sfmalloy@millersville.edu>
2020-12-06 10:09:29 -0500

    Day 6

---
+++ Day06/6.py
@@ -0,0 +1,20 @@
+with open('6.in') as f:
+  groups = [g.rstrip().split('\n') for g in f.read().split('\n\n')]
+
+total = 0
+yes = 0
+for g in groups:
+  unique = set()
+  lines = []
+  for l in g:
+    line = set()
+    for q in l:
+      unique.add(q)
+      line.add(q)
+    lines.append(line)
+  total += len(unique)
+  for l in lines:
+    unique &= l
+  yes += len(unique)    
+print(total)
+print(yes)
