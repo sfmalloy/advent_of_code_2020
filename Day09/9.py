da91d571d68e7583da670dff6b26e33ffc77c221
Sean Malloy <sfmalloy@millersville.edu>
2020-12-09 00:24:46 -0500

    Day 9

---
+++ Day09/9.py
@@ -0,0 +1,34 @@
+with open('9.in') as f:
+  lines = [int(x) for x in f.readlines()]
+
+N = 25
+invalid = 0
+preamble = lines[:N]
+for i in range(N, len(lines)):
+  found = False
+  for a in range(len(preamble)):
+    for b in range(a+1, len(preamble)):
+      if preamble[a]+preamble[b] == lines[i]:
+        found = True
+        break
+    if found:
+      break
+  if not found:
+    invalid = lines[i]
+    break
+  preamble.pop(0)
+  preamble.append(lines[i])
+print(invalid)
+
+sumlist = []
+s = 0
+for i in range(len(lines)):
+  if s < invalid:
+    sumlist.append(lines[i])
+    s += lines[i]
+  elif s == invalid:
+    break
+  while s > invalid:
+    s -= sumlist.pop(0)
+
+print(min(sumlist) + max(sumlist))
