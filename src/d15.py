e800c15844fbf452f4a66c8305df20c22a1f2c6e
Sean Malloy <sfmalloy@millersville.edu>
2020-12-15 11:04:44 -0500

    Day 15 using 2 dicts of ints instead of a dict of tuples

--- src/d15.py
+++ src/d15.py
@@ -3,24 +3,26 @@ import fileinput
 nums = list(map(int, fileinput.input().readline().split(',')))
 
 def play(nums, end):
+  prev_turns = {}
   turns = {}
   last_spoken = 0
   for t in range(1, end+1):
     if t < len(nums)+1:
-      # print(f'Turn {t}: Starting number {nums[t-1]}')
-      turns[nums[t-1]] = (0, t)
+      turns[nums[t-1]] = t
+      prev_turns[nums[t-1]] = 0
       last_spoken = nums[t-1]
-    elif turns[last_spoken][0] == 0:
-      # print(f'Turn {t}: {last_spoken} spoken for the first time. Next is 0')
+    elif prev_turns[last_spoken] == 0:
       last_spoken = 0
-      turns[0] = (turns[0][1], t)
+      prev_turns[0] = turns[0]
+      turns[0] = t
     else:
-      # print(f'Turn {t}: {last_spoken} spoken before. Next is {turns[last_spoken][1]}-{turns[last_spoken][0]}={turns[last_spoken][1] - turns[last_spoken][0]}')
-      last_spoken = turns[last_spoken][1] - turns[last_spoken][0]
+      last_spoken = turns[last_spoken] - prev_turns[last_spoken]
       if last_spoken in turns:
-        turns[last_spoken] = (turns[last_spoken][1], t)
+        prev_turns[last_spoken] = turns[last_spoken]
+        turns[last_spoken] = t
       else:
-        turns[last_spoken] = (0, t)
+        prev_turns[last_spoken] = 0
+        turns[last_spoken] = t
   return last_spoken
 
 print(play(nums, 2020))
