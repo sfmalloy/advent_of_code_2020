e387a42ecb2558542060e7b4a9a63862bbc9716e
Sean Malloy <sfmalloy@millersville.edu>
2020-12-19 17:10:49 -0500

    Modify run script

--- src/run.py
+++ src/run.py
@@ -3,11 +3,13 @@ import os
 import sys
 from optparse import OptionParser
 
+# Generate number with leading zero if less than 10
 def num_file(num):
   if int(num) < 10:
     return f'0{num}'
   return num
 
+# Run a single day given some file for input
 def run_single(file, day):
   if not os.path.exists(file):
     print(f'File {file} not found.')
@@ -18,16 +20,18 @@ def run_single(file, day):
   time = timeit.timeit(stmt=f'subprocess.run({args})', setup='import subprocess', number=1)
   print(f'Time: {round(1000*(time),3)}ms')
 
+# Run every day with input file specified as ../inputs/d<day_number>.in
 def run_all():
   num = 1
   fnum =  num_file(num)
   while os.path.exists(f'd{fnum}.py'):
     print(f'Day {num}:')
-    run_single(f'../input/d{fnum}', num)
+    run_single(f'../inputs/d{fnum}.in', num)
     num += 1
     fnum = num_file(num)
     print()
 
+# Add command line options
 parser = OptionParser()
 parser.add_option('-d', '--day', dest='day', help='Runs day <d>. If -f is not specified, '\
   'default uses input from inputs sibling directory.')
