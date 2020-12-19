cc6204b00783177c5d11d4e53834abff29bc92b6
Sean Malloy <sfmalloy@millersville.edu>
2020-12-19 17:05:39 -0500

    Day 19 cleanup

--- src/d19.py
+++ src/d19.py
@@ -20,7 +20,7 @@ for r in rules_lines:
     rules[int(num)] = subdefs
 
 g_idx = 0
-def check_str_p1(s, rule_num=0):
+def check_str(s, rule_num=0):
   global g_idx
   if rules[rule_num] == 'a' or rules[rule_num] == 'b':
     if g_idx >= len(s):
@@ -32,38 +32,18 @@ def check_str_p1(s, rule_num=0):
   for r in rules[rule_num]:
     t = g_idx
     for sr in r:
-      if not check_str_p1(s, sr):
+      if not check_str(s, sr):
         g_idx = t
         break
     if g_idx != t:
       return True
   return False
 
-# def check_str_p2(s, rule_num):
-#   global g_idx
-#   if rules[rule_num] == 'a' or rules[rule_num] == 'b':
-#     if g_idx >= len(s):
-#       yield False
-#     valid = rules[rule_num] == s[g_idx]
-#     if valid:
-#       g_idx += 1
-#     yield valid
-#   if rule_num == 8:
-#     for r in rules[rule_num]:
-#       t = g_idx
-#       for sr in r:
-#         if not check_str_p2(s, sr):
-#           g_idx = t
-#           break
-#       if g_idx != t:
-#         yield True
-#     yield False
-
 # Part 1
 ans = 0
 for case in cases:
   g_idx = 0
-  ans += check_str_p1(case) and g_idx == len(case)
+  ans += check_str(case) and g_idx == len(case)
 print(ans)
 
 rules[8] = [[42],[42,8]]
@@ -72,9 +52,9 @@ rules[11] = [[42,31],[42,11,31]]
 ans = 0
 for case in cases:
   g_idx = 0
-  while check_str_p1(case, 8):
+  while check_str(case, 8):
     temp_idx = g_idx
-    if check_str_p1(case, 11) and g_idx == len(case):
+    if check_str(case, 11) and g_idx == len(case):
       ans += 1
       break
 print(ans)
