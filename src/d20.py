9f5eafa8ce726321877ce8e8ff6526cbf00b7c1c
Sean Malloy <sfmalloy@millersville.edu>
2020-12-21 23:42:24 -0500

    Day 20 part 1 finished!

--- src/d20.py
+++ src/d20.py
@@ -1,141 +1,103 @@
 import fileinput
-from collections import namedtuple, deque
 from copy import deepcopy
-from math import sqrt
-from time import sleep
+from collections import namedtuple, deque
 
 class Tile:
-  def __init__(self, _id, image):
+  def __init__(self, _id, tile):
     self.id = _id
-    self.image = image
-  def __repr__(self):
-    return str(self)
-  def __str__(self):
-    s = ''
-    for r in self.image:
-      s += ''.join(r) + '\n'
-    return s
-  def __len__(self):
-    return len(self.image)
-  def flip_horiz(self):
-    i = 0
-    j = len(self.image)-1
-    while i < j:
-      for k in range(len(self.image)):
-        temp = self.image[k][i]
-        self.image[k][i] = self.image[k][j]
-        self.image[k][j] = temp
-      i += 1
-      j -= 1
-  def rotate(self):
-    self.image = list(map(list, zip(*self.image)))
+    self.tile = tile
+    self.angle = 0
+  def next_orient(self):
+    if self.angle == 3:
+      self.flip_horiz()
+    else:
+      self.rotate_cw()
+    self.angle += 1
+  def rotate_cw(self):
+    self.tile = list(map(list, zip(*self.tile)))
     self.flip_horiz()
-  def __getitem__(self, row):
-    return self.image[row]
-  def get_edges(self):
-    top_row = self.image[0]
-    bot_row = self.image[-1]
-    right_col = []
-    left_col = []
-    for r in range(len(self.image)):
-      left_col.append(self.image[r][0])
-      right_col.append(self.image[r][-1])
-    return (top_row, right_col, bot_row, left_col)
-  def compare_edges(self, other):
-    self_edges = self.get_edges()
-    other_edges = other.get_edges()
-    equal = set()
-    for i in range(len(self_edges)):
-      for j in range(len(other_edges)):
-        if self_edges[i] == other_edges[j]:
-          equal.add((i,j))
-    return equal
-class Grid:
-  def __init__(self, rows, cols):
-    self.grid = [[]]
-    self.rows = rows
-    self.cols = cols
-    self.curr_row = 0
-    self.curr_col = -1
-    self.ids = set()
-  def __len__(self):
-    return sum(len(row) for row in self.grid)
+  def flip_horiz(self):
+    for i in range(len(self.tile)):
+      a = 0
+      b = len(self.tile)-1
+      while a < b:
+        t = self.tile[i][a]
+        self.tile[i][a] = self.tile[i][b]
+        self.tile[i][b] = t
+        a += 1
+        b -= 1
+  def equal_rows(self, other):
+    return self[-1] == other[0]
+  def equal_cols(self, other):
+    for i in range(len(self.tile)):
+      if self[i][-1] != other[i][0]:
+        return False
+    return True
+  def __getitem__(self, idx):
+    return self.tile[idx]
   def __repr__(self):
-    return str(self)
-  def __str__(self):
     s = ''
-    for i in range(self.rows):
-      for r in range(len(self.grid[0][0])):
-        for j in range(self.cols):
-          try:
-            s += ''.join(self.grid[i][j][r]) + ' '
-          except IndexError:
-            break
-        s += '\n'
+    for r in self.tile:
+      for c in r:
+        s += c
       s += '\n'
     return s
-  def __getitem__(self, idx):
-    return self.grid[idx]
-  def add(self, tile):
-    if self.curr_col == self.cols-1:
-      self.curr_row += 1
-      self.curr_col = -1
-      self.grid.append([])
-    self.grid[self.curr_row].append(tile)
-    self.curr_col += 1
-    self.ids.add(tile.id)
-  def get_curr_tile(self):
-    return self.grid[self.curr_row][self.curr_col]
 
 tiles = deque()
-curr_tile = []
 curr_id = 0
+curr_tile = []
 for line in fileinput.input():
   line = line.strip()
-  if len(line) == 0:
-    tiles.append(Tile(curr_id, curr_tile))
-    curr_tile = []
-  elif line[0] == 'T':
-    curr_id = int(line.split()[1][:-1])
+  if len(line) > 0:
+    if line[0] == 'T':
+      curr_id = int(line[:-1].split()[1])
+      curr_tile = []
+    else:
+      curr_tile.append([c for c in line])
   else:
-    curr_tile.append([c for c in line.strip()])
+    new_tile = [[c for c in r] for r in curr_tile]
+    t = Tile(curr_id, new_tile)
+    for _ in range(8):
+      tiles.append(deepcopy(t))
+      t.next_orient()
 
-grid_len = int(sqrt(len(tiles)))
-grid_total = len(tiles)
+grid_size = len(tiles) // 8
+col_size = int(grid_size**(1/2))
 
+node = namedtuple('node', 'row col grid ids')
 frontier = deque()
 
-all_tiles = deque()
-init = Grid(grid_len, grid_len)
-for i in range(len(tiles)):
-  for j in range(8):
-    init_copy = deepcopy(init)
-    if j > 0:
-      if j == 4:
-        tiles[i].flip_horiz()
-      else:
-        tiles[i].rotate()
-    init_copy.add(deepcopy(tiles[i]))
-    frontier.append(init_copy)
-    all_tiles.append(deepcopy(tiles[i]))
-  tiles.append(tiles.popleft())
+for t in tiles:
+  grid = [[None for _ in range(col_size)] for _ in range(col_size)]
+  grid[0][0] = deepcopy(t)
+  ids = {t.id}
+  frontier.append(node(0, 0, grid, ids))
+
+full_cols = set(col_size*i for i in range(1,col_size))
 
 solved = None
-front = None
-valid = False
-while not valid:
+while True:
   front = frontier.pop()
-  for i in range(len(all_tiles)):
-    if all_tiles[i].id not in front.ids:
-      if (1, 3) in front.get_curr_tile().compare_edges(all_tiles[i]) or len(front[front.curr_row]) == front.cols:
-        front_copy = deepcopy(front)
-        front_copy.add(deepcopy(all_tiles[i]))
-        if front_copy.curr_row == 0 or (0, 2) in all_tiles[i].compare_edges(front_copy[front_copy.curr_row-1][front_copy.curr_col]):
-          frontier.append(front_copy)
-          if len(front_copy) == grid_total:
-            valid = True
-            solved = front_copy
-            break
+  if len(front.ids) < grid_size:
+    for i in range(len(tiles)):
+      if tiles[i].id not in front.ids:
+        valid = True
+        if len(front.ids) < col_size or len(front.ids) not in full_cols:
+          valid = front.grid[front.row][front.col].equal_rows(tiles[i])
+        if valid:
+          row = front.row + 1
+          col = front.col
+          if row > col_size-1:
+            row = 0
+            col = front.col + 1
+          if col > 0:
+            valid = front.grid[row][col-1].equal_cols(tiles[i])
+          if valid:
+            grid_copy = deepcopy(front.grid)
+            grid_copy[row][col] = deepcopy(tiles[i])
+            frontier.append(node(row, col, grid_copy, front.ids | {tiles[i].id}))
+  else:
+    solved = front.grid
+    break
 prod = solved[0][0].id * solved[0][-1].id * solved[-1][0].id * solved[-1][-1].id
 print(prod)
-print(solved)
